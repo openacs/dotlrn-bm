ef82b776cb4031d5088a34ee99b6c21b14a7cec5
Dan Chak <chak@mit.edu>
2002-07-15 20:43:41 +0000

    datamodels ported by mohan pakkurti and samir joshi

---
+++ sql/postgresql/dotlrn-bm-create.sql
@@ -0,0 +1,137 @@
+--
+--  Copyright(C) 2001, 2002 OpenForce, Inc.
+--
+--  This file is part of dotLRN.
+--
+--  dotLRN is free software; you can redistribute it and/or modify it under the
+--  terms of the GNU General Public License as published by the Free Software
+--  Foundation; either version 2 of the License, or(at your option) any later
+--  version.
+--
+--  dotLRN is distributed in the hope that it will be useful, but WITHOUT ANY
+--  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+--  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+--  details.
+--
+
+--
+-- Creates the bulk-mail portlet
+--
+-- @author yon (yon@openforce.net)
+-- @creation-date 2002-05-13
+-- @version $Id$
+--
+-- Postgresql port adarsh@symphinity.com
+--  
+-- 11th July 2002
+--
+
+select acs_sc_impl__new(
+        'dotlrn_applet',					-- impl contract name
+        'dotlrn_bm',						-- impl name 
+        'dotlrn_bm'							-- impl owner name
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',					-- impl contract name
+        'dotlrn_bm',						-- impl name 
+        'GetPrettyName',					-- impl operation name
+        'dotlrn_bm::get_pretty_name',		-- impl alias
+        'TCL'								-- impl pl
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddApplet',
+        'dotlrn_bm::add_applet',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveApplet',
+        'dotlrn_bm::remove_applet',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddAppletToCommunity',
+        'dotlrn_bm::add_applet_to_community',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveAppletFromCommunity',
+        'dotlrn_bm::remove_applet_from_community',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddUser',
+        'dotlrn_bm::add_user',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveUser',
+        'dotlrn_bm::remove_user',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddUserToCommunity',
+        'dotlrn_bm::add_user_to_community',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveUserFromCommunity',
+        'dotlrn_bm::remove_user_from_community',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddPortlet',
+        'dotlrn_bm::add_portlet',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemovePortlet',
+        'dotlrn_bm::remove_portlet',
+        'TCL'
+);
+
+select acs_sc_impl_alias__new(
+        'dotlrn_applet',     
+        'dotlrn_bm',
+        'Clone',
+        'dotlrn_bm::clone',
+        'TCL'
+);
+
+select acs_sc_binding__new(
+        'dotlrn_applet',				--contract name			     	  
+        'dotlrn_bm'						--impl name					   
+);
+
+
+
---
+++ sql/postgresql/dotlrn-bm-drop.sql
@@ -0,0 +1,110 @@
+--
+--  Copyright(C) 2001, 2002 OpenForce, Inc.
+--
+--  This file is part of dotLRN.
+--
+--  dotLRN is free software; you can redistribute it and/or modify it under the
+--  terms of the GNU General Public License as published by the Free Software
+--  Foundation; either version 2 of the License, or(at your option) any later
+--  version.
+--
+--  dotLRN is distributed in the hope that it will be useful, but WITHOUT ANY
+--  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+--  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+--  details.
+--
+--
+-- Drops the bulk-mail portlet
+--
+-- @author yon (yon@openforce.net)
+-- @creation-date 2002-05-13
+-- @version $Id$
+--
+-- Postgresql port adarsh@symphinity.com
+--  
+-- 11th July 2002
+--
+
+select acs_sc_impl__delete(
+        'dotlrn_applet',					-- impl contract name
+        'dotlrn_bm'	 						-- impl name 
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',					-- impl contract name
+        'dotlrn_bm',						-- impl name 
+        'GetPrettyName'						-- impl operation name
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddApplet'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveApplet'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddAppletToCommunity'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveAppletFromCommunity'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddUser'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveUser'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddUserToCommunity'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemoveUserFromCommunity'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'AddPortlet'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',
+        'dotlrn_bm',
+        'RemovePortlet'
+);
+
+select acs_sc_impl_alias__delete(
+        'dotlrn_applet',     
+        'dotlrn_bm',
+        'Clone'
+);
+
+select acs_sc_binding__delete(
+        'dotlrn_applet',			     	   --contract name
+        'dotlrn_bm'							   --impl name
+);
+
+
