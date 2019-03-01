071f1a5deaf2253c5ed30468cb2d73b7698e99c3
Antonio Pisano <antonio@elettrotecnica.it>
2019-03-01 11:23:18 +0000

    Replace ad_decode idioms

--- www/admin/index.tcl
+++ www/admin/index.tcl
@@ -48,7 +48,7 @@ template::list::create -name messages \
 db_multirow -extend {send_date_pretty message_url status_pretty} message_list get_mail_messages {} {
     set send_date_pretty [lc_time_fmt $send_date %q]
     set message_url "[lindex [site_node::get_url_from_object_id -object_id $package_id] 0]one?bulk_mail_id=$bulk_mail_id"
-    set status_pretty [ad_decode $status sent [_ bulk-mail.Sent] pending [_ bulk-mail.Pending] [_ bulk-mail.Cancelled]]
+    set status_pretty [switch $status { "sent" {_ bulk-mail.Sent} "pending" {_ bulk-mail.Pending} default {_ bulk-mail.Cancelled} }]
 }
 
 ad_return_template
